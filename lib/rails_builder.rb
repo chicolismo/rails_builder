e85f9a0be208f46e512c111baf6c040006b8bae6
Carlos Francisco Pinheiro <carlosfpinheiro@gmail.com>
2023-07-13 00:04:46 -0300

    First commit

---
+++ lib/rails_builder.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_builder/version"
+
+module RailsBuilder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rails_builder_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsBuilder do
+  it "has a version number" do
+    expect(RailsBuilder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
